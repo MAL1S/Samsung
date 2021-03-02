243f5c2ab0b9bfa9c7f53b735cd6ac7e3b5c781d
MAL1S <MAL1S@users.noreply.github.com>
2021-03-03 00:15:54 +0800

    new project

---
+++ Samsung341/app/src/main/java/com/example/samsung341/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.samsung341;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.text.Html;
+import android.text.Layout;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    ImageView imageView;
+    String url;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        url = "https://i.pinimg.com/originals/5f/72/33/5f7233081a90c4d8065c834718084522.jpg";
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new LoadImage().execute(url);
+            }
+        });
+    }
+
+    private class LoadImage extends AsyncTask<String, Integer, Bitmap> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            String url = urls[0];
+            Bitmap bitmap = null;
+            try {
+                bitmap = BitmapFactory.decodeStream(new URL(url).openStream());
+            } catch (IOException e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        @Override
+        protected void onPostExecute(Bitmap image) {
+            imageView.setImageBitmap(image);
+        }
+    }
+}
